9016ccd51e50288f15f4a7ceefa2121b87639799
Starskii <jakeylakie@yahoo.com>
2024-08-31 20:31:02 -0700

    added click and drag feature

--- index.js
+++ index.js
@@ -1,20 +1,63 @@
-const container = document.querySelector(".container");
+const container = document.querySelector("#container");
+const resetButtonNode = document.querySelector("#reset-button");
+const resizeButtonNode = document.querySelector("#resize-button");
+
+let width = 16;
+let height = 16; 
+let drawState = false;
+
+const clearBox = () => {
+    drawBox(width, height);
+}
+
+const drawBox = (width, height) => {
+    container.innerHTML = "";
+
+    const COLUMNS = width;
+    const ROWS = height;
+    
+    for (let col = 0; col < COLUMNS; col++){
+        const COLUMN_NODE = document.createElement("div");
+        COLUMN_NODE.classList.add("column")
+        for (let row = 0; row < ROWS; row++){
+            const ROW_NODE = document.createElement("div");
+            ROW_NODE.classList.add("row"); 
+            ROW_NODE.addEventListener("mouseover", elementClicked);
+            COLUMN_NODE.appendChild(ROW_NODE);
+        }
+        container.appendChild(COLUMN_NODE);
+    }
+} 
 
 const elementClicked = (e) => {
-    console.log(e.target);
+    if(drawState)
+        e.target.classList.add("filled");
 }
 
-const COLUMNS = 16;
-const ROWS = 16;
+const resizeButtonClicked = () => {
+    do{
+        width = prompt("How many tiles wide? (Provide integer value 0 < x <= 100)");
+    } while(parseInt(width) <= 0 || parseInt(width) > 100 || !parseInt(width));
 
-for (let col = 0; col < COLUMNS; col++){
-    const COLUMN_NODE = document.createElement("div");
-    COLUMN_NODE.classList.add("column")
-    for (let row = 0; row < ROWS; row++){
-        const ROW_NODE = document.createElement("div");
-        ROW_NODE.classList.add("row"); 
-        ROW_NODE.addEventListener("click", elementClicked);
-        COLUMN_NODE.appendChild(ROW_NODE);
-    }
-    container.appendChild(COLUMN_NODE);
+    do{
+        height = prompt("How many tiles high? (Provide integer value 0 < x <= 100)");
+    } while(parseInt(height) <= 0 || parseInt(height) > 100 || !parseInt(height));
+    drawBox(width, height);
 }
+
+resetButtonNode.addEventListener("click", clearBox);
+resizeButtonNode.addEventListener("click", resizeButtonClicked);
+container.addEventListener("mousedown", function(e) {
+    e.preventDefault();
+    if(e.button===0)
+        drawState = true;
+    console.log(drawState);
+});
+
+container.addEventListener("mouseup", function(e) {
+    e.preventDefault();
+    if(e.button===0)
+        drawState = false;
+});
+
+drawBox(16, 16);
